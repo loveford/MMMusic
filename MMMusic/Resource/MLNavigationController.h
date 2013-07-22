33ac71a088abdbe8f2db8297668009e87a254be5
loveford <loveford@vip.qq.com>
2013-07-22 17:27:56 +0800

    add new slider

---
+++ MMMusic/Resource/MLNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  Created by mouwenbin on 4/20/13.
+//  Copyright (c) 2013 mouwenbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLNavigationController : UINavigationController
+
+// Enable the drag to back interaction, Defalt is YES.
+@property (nonatomic,assign) BOOL canDragBack;
+
+@end
